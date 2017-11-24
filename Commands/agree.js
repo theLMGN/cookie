ace37866cbbee95bb05836745823d799b2e23d6c
BuildTools <SunburntRock89@gmail.com>
2017-11-24 23:30:21 +0000

    ffs this is painful

--- Commands/agree.js
+++
@@ -1,6 +0,0 @@
-const Discord = require("discord.js");
-
-module.exports = async(client, message, suffix, serverDocument) => {
-	message.reply(":white_check_mark:");
-	message.member.addRole("name", "Member");
-};
